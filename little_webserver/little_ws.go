47449cbb41ae140a9797bd2186262db93d0bfa97
Samuel Salcedo Orellana <s.salcedo.orellana@gmail.com>
2016-05-22 15:11:52 +0200

    Añadido pass en claro

--- little_webserver/little_ws.go
+++ little_webserver/little_ws.go
@@ -6,15 +6,28 @@ import (
 	"net/http"
 )
 
+func checkPassword(pwd string) bool {
+	return pwd == "1234"
+}
+
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("New connection from " + req.RemoteAddr + "\n")
-	w.Header().Set("Trailer", "AtEnd1, AtEnd2")
-	w.Header().Add("Trailer", "AtEnd3")
+	fmt.Printf("Method: " + req.Method + "\n")
+	fmt.Printf("Password: " + req.FormValue("password") + "\n")
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
 	w.WriteHeader(http.StatusOK)
 
-	io.WriteString(w, "This HTTP response is a hello world.\n")
+	checkPwd := checkPassword(req.FormValue("password"))
+	if checkPwd {
+		io.WriteString(w, "Valid password\n")
+	} else {
+		io.WriteString(w, "Invalid password\n")
+	}
+	//w.Header().Set("Trailer", "AtEnd1, AtEnd2")
+	//w.Header().Add("Trailer", "AtEnd3")
+
+	//io.WriteString(w, "This HTTP response is a hello world.\n")
 	//fmt.Fprintf(w, "This HTTP response has both headers before this text and trailers at the end.\n")
 }
 
